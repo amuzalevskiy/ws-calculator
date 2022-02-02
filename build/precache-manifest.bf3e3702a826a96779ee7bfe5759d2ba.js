0c19a72560c182574c964ab65585738e13a0b74b
amuzalevskyi <face2world@gmail.com>
2022-02-02 12:46:51 +0200

    Add built files, have no memory to complete build process on AWS nano instance

---
+++ build/precache-manifest.bf3e3702a826a96779ee7bfe5759d2ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d12598dfb4b34d29c49d4434e882e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e2c11f1b4a4193ef97f",
+    "url": "/static/css/main.81f91179.chunk.css"
+  },
+  {
+    "revision": "9f872d09a454cc3e2173",
+    "url": "/static/js/2.bec1f00e.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/static/js/2.bec1f00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2c11f1b4a4193ef97f",
+    "url": "/static/js/main.04af5084.chunk.js"
+  },
+  {
+    "revision": "7714f4bde7fab7635dee",
+    "url": "/static/js/runtime-main.c220b2e8.js"
+  }
+]);
