27cd6c97ccbd4b09b2d5b66d1ebea3cc0653aeff
amuzalevskyi <face2world@gmail.com>
2022-02-03 13:27:14 +0200

    Better mining calculation

---
+++ build/precache-manifest.c9f74d94b7f7bb03ca65d4c0bce03bbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cb83c7d8298877818ddbcb15911882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffe4fbc01d89c48ba0c9",
+    "url": "/static/css/main.058e27e6.chunk.css"
+  },
+  {
+    "revision": "1c5faaccb8b9cff3c3ee",
+    "url": "/static/js/2.0aec3870.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/static/js/2.0aec3870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe4fbc01d89c48ba0c9",
+    "url": "/static/js/main.c783165f.chunk.js"
+  },
+  {
+    "revision": "7714f4bde7fab7635dee",
+    "url": "/static/js/runtime-main.c220b2e8.js"
+  }
+]);
